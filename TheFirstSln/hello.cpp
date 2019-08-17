5b2f78c4d7b076422ecd70c1d3715648b0d6edf1
mystic-cg <215104920@qq.com>
2019-08-17 14:39:03 +0800

    learning local and global variable

--- TheFirstSln/hello.cpp
+++ TheFirstSln/hello.cpp
@@ -2,49 +2,66 @@
 
 // define int variable, and assign 4 directly
 extern int aa = 4;
+int cc = 123;
+
+// define function
+int ret();
 
 /*
 parameter is none
 return is int
 */
 int main(void) {
-	// only define external variable on a local declaration
-	// cannot initialize directly
-	extern int bb;
+    // only define external variable on a local declaration
+    // cannot initialize directly
+    extern int bb;
 
-	// print hello, world
-	std::cout << "hello, world" << std::endl;
+    // print hello, world
+    std::cout << "hello, world" << std::endl;
 
-	bool is_on = false;
-	char ch = 'a';
-	int num = 10;
-	float fl = 10.1;
-	double dou = 10.2;
-	std::cout << "num + fl = " << num + (int)fl << std::endl;
+    bool is_on = false;
+    char ch = 'a';
+    int num = 10;
+    float fl = 10.1;
+    double dou = 10.2;
+    std::cout << "num + fl = " << num + (int)fl << std::endl;
 
-	// define boolean 
-	typedef bool boolean;
-	boolean is_Okay = false;
+    // define boolean 
+    typedef bool boolean;
+    boolean is_Okay = false;
 
-	enum month
-	{
-		Jan, // 0, enum is from 0, then autoincrease +1
-		Feb = 2,
-		March, // 3, it's up to the previous value
-	};
+    enum month
+    {
+        Jan, // 0, enum is from 0, then autoincrease +1
+        Feb = 2,
+        March, // 3, it's up to the previous value
+    };
 
-	// define enum month variable "mon", whose value is March
-	enum month mon = March;
+    // define enum month variable "mon", whose value is March
+    enum month mon = March;
 
-	// define enum weekday variable "day"
-	enum weekday
-	{
-		Sun, //0
-		Mon, //1
-		Tues, //2
-		Wed, Thur, Fri, Sat
-	} day;
-	day = Fri;
+    // define enum weekday variable "day"
+    enum weekday
+    {
+        Sun, //0
+        Mon, //1
+        Tues, //2
+        Wed, Thur, Fri, Sat
+    } day;
+    day = Fri;
 
-	return 0;
+    int cc = 20;
+    // the func's cc is still global cc, so kk = 123
+    int kk = ret();
+    // local cc
+    std::cout << cc << std::endl;
+    // global cc
+    std::cout << ::cc << std::endl;
+    std::cout << kk << std::endl;
+
+    return 0;
+}
+
+int ret() {
+    return cc;
 }
