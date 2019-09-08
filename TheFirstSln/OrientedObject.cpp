689295627e4a2d6e1849cfaca3c4a1f19104e4ce
mystic-cg <215104920@qq.com>
2019-09-08 12:54:45 +0800

    [learning]

--- TheFirstSln/OrientedObject.cpp
+++ TheFirstSln/OrientedObject.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 class Box {
+public:
+    double get_length() { return length; }
+private:
     double length;
     double width;
     double height;
-
-public:
-    double get_length() { return length; }
 } box1, box2;
 
-int main(void) { cout << box1.get_length << endl; }
+int main(void) {
+    cout << box1.get_length << endl;
+}
