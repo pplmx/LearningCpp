1ce0c23c7eb54da3e8635df3e51e33872eeb2e08
mystic-cg <215104920@qq.com>
2019-09-28 20:35:18 +0800

    [learning]

--- TheFirstSln/graph.cpp
+++ TheFirstSln/graph.cpp
@@ -42,15 +42,18 @@ GraphUtils::GraphUtils(set<edge_tuple> edge_set)
         u_idx = this->locate_vertex(this->graph, u);
         v_idx = this->locate_vertex(this->graph, v);
 
-        // init edge to be not visited
+        // init edge to be not visited and set the weight of a edge
         edge_ptr->is_visited = false;
+        edge_ptr->weight = 0;
 
         edge_ptr->u_idx = u_idx;
         edge_ptr->u_next_edge = this->graph->list[u_idx].first_edge;
+        // point to this new edge on u
         this->graph->list[u_idx].first_edge = edge_ptr;
 
         edge_ptr->v_idx = v_idx;
         edge_ptr->v_next_edge = this->graph->list[v_idx].first_edge;
+        // point to this new edge on v
         this->graph->list[v_idx].first_edge = edge_ptr;
     }
 }
@@ -69,6 +72,43 @@ GraphUtils::GraphUtils(set<edge_w_tuple> edge_set)
     this->graph = new Graph;
     this->graph->vertex_num = this->vertex_set.size();
     this->graph->edge_num = this->edge_w_set.size();
+    this->graph->list = new Vertex[this->graph->vertex_num];
+
+    // init adjacency multi-list
+    // init all vertices' struct in adj_multi_list
+    size_t idx = 0;
+    for (auto vertex : this->vertex_set) {
+        this->graph->list[idx].data = vertex;
+        this->graph->list[idx].first_edge = nullptr;
+        this->graph->list[idx].threshold = 0;
+        idx++;
+    }
+    // init all edges' struct in adj_multi_list
+    VertexType u, v;
+    int u_idx, v_idx;
+    for (auto edge : this->edge_w_set) {
+        Edge* edge_ptr = new Edge;
+        u = get<0>(edge);
+        v = get<1>(edge);
+
+        // find the index of a node in adj_multi_list
+        u_idx = this->locate_vertex(this->graph, u);
+        v_idx = this->locate_vertex(this->graph, v);
+
+        // init edge to be not visited and set the weight of a edge
+        edge_ptr->is_visited = false;
+        edge_ptr->weight = get<2>(edge);
+
+        edge_ptr->u_idx = u_idx;
+        edge_ptr->u_next_edge = this->graph->list[u_idx].first_edge;
+        // point to this new edge on u
+        this->graph->list[u_idx].first_edge = edge_ptr;
+
+        edge_ptr->v_idx = v_idx;
+        edge_ptr->v_next_edge = this->graph->list[v_idx].first_edge;
+        // point to this new edge on v
+        this->graph->list[v_idx].first_edge = edge_ptr;
+    }
 }
 
 GraphUtils::GraphUtils(const GraphUtils& obj)
@@ -177,27 +217,38 @@ void GraphUtils::set_edge_set(set<edge_w_tuple> edge_set)
 
 int main()
 {
+    // use the compare funtion of set
     // set<tuple<int, int>, decltype(&edge_compare)> edge_set(&edge_compare);
-    // set<edge_tuple> edge_set;
-    set<edge_tuple> edge_set;
+    /*set<edge_tuple> edge_set;
     edge_set.insert(make_tuple(2, 3));
     edge_set.insert(make_tuple(1, 2));
     edge_set.insert(make_tuple(2, 1));
     edge_set.insert(make_tuple(2, 3));
     edge_set.insert(make_tuple(3, 4));
     edge_set.insert(make_tuple(4, 5));
-    edge_set.insert(make_tuple(2, 6));
+    edge_set.insert(make_tuple(2, 6));*/
+
+    set<edge_w_tuple> edge_set;
+    edge_set.insert(make_tuple(2, 3, 0.2));
+    edge_set.insert(make_tuple(1, 2, 0.4));
+    edge_set.insert(make_tuple(2, 1, 0.1));
+    edge_set.insert(make_tuple(2, 3, 0.7));
+    edge_set.insert(make_tuple(3, 4, 0.9));
+    edge_set.insert(make_tuple(4, 5, 0.1));
+    edge_set.insert(make_tuple(2, 6, 0.5));
 
     GraphUtils graph(edge_set);
     set<VertexType> vs = graph.get_vertex_set();
-    set<edge_tuple> es = graph.get_edge_set();
+    // set<edge_tuple> es = graph.get_edge_set();
+    set<edge_w_tuple> es = graph.get_edge_w_set();
     set<VertexType> nbr_node = graph.get_neighbor_node_set(2);
     for_each(vs.begin(), vs.end(), [](const auto& vertex) {
         std::cout << vertex << ", ";
     });
     std::cout << std::endl;
     for_each(es.begin(), es.end(), [](const auto& edge) {
-        std::cout << "(" << get<0>(edge) << ", " << get<1>(edge) << ")" << std::endl;
+        // std::cout << "(" << get<0>(edge) << ", " << get<1>(edge) << ")" << std::endl;
+        std::cout << "(" << get<0>(edge) << ", " << get<1>(edge) << ", " << get<2>(edge) << ")" << std::endl;
     });
     for_each(nbr_node.begin(), nbr_node.end(), [](const auto& vertex) {
         std::cout << vertex << ", ";
